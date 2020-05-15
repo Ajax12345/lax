c1370f843cb6f41da8d2c62580c9a4fdbdfb97ad
= <jpetullo@worcester.edu>
2020-05-15 17:01:37 -0400

    exceptions and functions

--- lax.py
+++ lax.py
@@ -1,9 +1,57 @@
 import typing, json, abc
 import drivers.lax_drivers as lax_drivers
-import lax_utils
+import lax_utils, collections
+
+class funcs:
+    class dbFuncs:
+        @property
+        def hook(self) -> typing.NamedTuple:
+            hook = collections.namedtuple('hook', ('catch', 'name'))
+            return hook((k:=self.__class__.__name__).lower(), k)
+        def __repr__(self) -> str:
+            return f'<dbFunc {self.__class__.__name__}({self.column})'
+    
+    class AVE(dbFuncs):
+        __slots__ = ('column')
+        @lax_utils.enforce_column
+        def __init__(self, column:typing.Optional[str]=None):
+            self.column = column
+
+    class COUNT(dbFuncs):
+        __slots__ = ('column')
+        def __init__(self, column:typing.Optional[str]=None):
+            self.column = column
+
+    class MAX(dbFuncs):
+        __slots__ = ('column')
+        @lax_utils.enforce_column
+        def __init__(self, column:typing.Optional[str]=None):
+            self.column = column
+
+    class MIN(dbFuncs):
+        __slots__ = ('column')
+        @lax_utils.enforce_column
+        def __init__(self, column:typing.Optional[str]=None):
+            self.column = column
+
+    class SUM(dbFuncs):
+        __slots__ = ('column')
+        @lax_utils.enforce_column
+        def __init__(self, column:typing.Optional[str]=None):
+            self.column = column        
+
 
 class SELECT:
-    pass
+    __slots__ = ('tablename', 'args', 'where', 'distinct', 'limit', 'bindings')
+    def __init__(self, tablename:str, *args:typing.List[typing.Any], where:typing.Optional = None, distinct:typing.Optional[bool]=None, limit:typing.Optional[int]=None, bindings:typing.Optional[typing.List[str]]=None) -> None:
+        self.tablename, self.args, self.where, self.distinct, self.limit, self.bindings = tablename, args, where, distinct, limit, bindings
+    
+    def __repr__(self) -> str:
+        return f'<lax RAW {self.__class__.__name__} header'
+
+    @property
+    def hook(self) -> str:
+        return self.__class__.__name__
 
 class UPDATE:
     pass
@@ -14,12 +62,13 @@ class CREATE:
 class INSERT:
     pass
 
-class DBCallback:
+class DELETE:
     pass
+
 
 class LaxMain(abc.ABC):
     @abc.abstractmethod
-    def execute(self, expression:typing.Union[SELECT, UPDATE, CREATE, INSERT]) -> DBCallback:
+    def execute(self, expression:typing.Union[SELECT, UPDATE, CREATE, INSERT, DELETE]) -> typing.Callable:
         """run an @expression object"""
 
     
@@ -30,8 +79,9 @@ class Lax(LaxMain):
         self.conn = None
 
     @lax_utils.load_conn
-    def execute(self, conn:typing.Union[lax_drivers.SQLite, lax_drivers.MySQL], expression:typing.Union[SELECT, UPDATE, CREATE, INSERT]) -> DBCallback:
-        pass
+    def execute(self, conn:typing.Union[lax_drivers.SQLite, lax_drivers.MySQL], expression:typing.Union[SELECT, UPDATE, CREATE, INSERT, DELETE]) -> typing.Callable:
+        return conn.execute(expression)
+    
 
     
     
--- lax_utils.py
+++ lax_utils.py
@@ -1,4 +1,4 @@
-import typing, functools
+import typing, functools, lax_exceptions
 
 
 
@@ -10,4 +10,12 @@ def load_conn(f:typing.Callable) -> typing.Callable:
                 return f(self, ex_db, _expression)
         return f(self, self.conn, _expression)
     return _execute
+
+def enforce_column(f:typing.Callable) -> typing.Callable:
+    @functools.wraps(f)
+    def _validate(_self, column=None) -> None:
+        if column is None:
+            raise lax_exceptions.MissingColumn(f"function '{_self.__class__.__name__}' requires a column")
+        return f(_self, column)
+    return _validate
 
